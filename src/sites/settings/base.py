55a8987e5de470b4e649894f81e9a8719f71c6ee
jh.park <jh.park@ridi.com>
2019-10-29 01:15:07 +0900

    sql 파일을 root_path 에 저장하도록 함

--- src/sites/settings/base.py
+++ src/sites/settings/base.py
@@ -57,7 +57,7 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_PATH, 'db.sqlite3'),
+        'NAME': os.path.join(ROOT_PATH, 'db.sqlite3'),
     }
 }
 
